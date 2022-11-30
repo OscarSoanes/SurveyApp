40a6a1ae0c8aa41992e4aaac56d7379e0d5584ea
Oscar Soanes <oscarsoanes@gmail.com>
2022-11-30 16:55:47 +0000

    Validation added, sign up working as intended

--- app/src/main/java/com/example/surveyapp/SignupActivity.kt
+++ app/src/main/java/com/example/surveyapp/SignupActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.Toast
 import com.example.surveyapp.model.Admin
 import com.example.surveyapp.model.DataBaseHelper
+import com.example.surveyapp.model.Student
 
 class SignupActivity : AppCompatActivity() {
     var globalRole = ""
@@ -26,18 +27,47 @@ class SignupActivity : AppCompatActivity() {
         // validation
         if (userName.isEmpty() || password.isEmpty()) {
             Toast.makeText(applicationContext,"Username and password are required!", Toast.LENGTH_LONG).show()
+            return
         }
+
+        if (userName.length < 3) {
+            Toast.makeText(applicationContext, "Username must be greater than 3 characters", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (password.length < 6 && !password.contains("[0-9]".toRegex())) {
+            Toast.makeText(applicationContext, "Password is too easy to guess", Toast.LENGTH_LONG).show()
+            return
+        }
+
         // TODO (CREATE VALIDATION RULES)
+        if (globalRole == "Admin") {
+            val newAdmin = Admin(-1, userName, password)
+            val database = DataBaseHelper(this)
 
-        val newAdmin = Admin(-1, userName, password)
+            when (database.addAdmin(newAdmin)) {
+                1 -> {
+                    Toast.makeText(applicationContext, "Working", Toast.LENGTH_LONG).show()
+                    // TODO GOTO ADMIN PANEL (PASS ADMIN ID PLEASE)
+                }
+                -1 -> Toast.makeText(applicationContext, "Error creating new account", Toast.LENGTH_LONG).show()
+                -2 -> Toast.makeText(applicationContext, "Cannot open database", Toast.LENGTH_LONG).show()
+                -3 -> Toast.makeText(applicationContext, "Username already exists", Toast.LENGTH_LONG).show()
+            }
+            return
+        }
+        val newStudent = Student(-1, userName, password)
         val database = DataBaseHelper(this)
-        val result = database.addAdmin(newAdmin)
 
-        when (result) {
-            1 -> Toast.makeText(applicationContext, "Working", Toast.LENGTH_LONG).show()
+        when (database.addStudent(newStudent)) {
+            1 -> {
+                Toast.makeText(applicationContext, "Working", Toast.LENGTH_LONG).show()
+                // TODO GOTO STUDENT PANEL (PASS STUDENT ID PLEASE)
+            }
             -1 -> Toast.makeText(applicationContext, "Error creating new account", Toast.LENGTH_LONG).show()
             -2 -> Toast.makeText(applicationContext, "Cannot open database", Toast.LENGTH_LONG).show()
             -3 -> Toast.makeText(applicationContext, "Username already exists", Toast.LENGTH_LONG).show()
         }
+
     }
 }
