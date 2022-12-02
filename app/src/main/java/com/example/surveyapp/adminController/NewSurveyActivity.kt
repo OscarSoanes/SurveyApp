41828f8babad4a376ccb461ec834fa90a7cfc8a0
Oscar Soanes <oscarsoanes@gmail.com>
2022-12-02 18:10:36 +0000

    Updated date creation on new survey, validated all inputs

--- app/src/main/java/com/example/surveyapp/adminController/NewSurveyActivity.kt
+++ app/src/main/java/com/example/surveyapp/adminController/NewSurveyActivity.kt
@@ -3,11 +3,17 @@ package com.example.surveyapp.adminController
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import com.example.surveyapp.R
 import com.example.surveyapp.model.Survey
 import java.lang.Integer.parseInt
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class NewSurveyActivity : AppCompatActivity() {
     private var globalId = 0
@@ -20,11 +26,112 @@ class NewSurveyActivity : AppCompatActivity() {
     }
 
     fun btnSave(view: View) {
-        val title = findViewById<EditText>(R.id.editTextTitle).text.toString()
-        val startDate = findViewById<EditText>(R.id.editTextStartDate).text.toString()
-        val endDate = findViewById<EditText>(R.id.editTextEndDate).text.toString()
+        fun validation(day: Int, month: Int, year: Int, position: String, currentYear: Int): String {
+            var message = ""
+            // Checking year
+            if (year >= currentYear+5) {
+                message = "$position year cannot be set greater than 5 years from now"
+            }
+            // Checking months
+            if (month !in 1 .. 12) {
+                message = "$position month is invalid"
+            }
 
-        // TODO VALIDATION ON DATES
+            // Checking days
+            if (month % 2 == 1) { // JAN, MAR, MAY, JUL, SEP, NOV
+                if (day !in 1 .. 31) {
+                    message = "$position day is invalid"
+                }
+            } else if (month % 2 == 0 && month != 2) { // APR, JUN, AUG, OCT, DEC
+                if (day !in 1 .. 30) {
+                    message = "$position day is invalid"
+                }
+            } else { // FEB
+                if (year % 4 != 0) { // NOT LEAP YEAR
+                    if (day !in 1 .. 28) {
+                        message = "$position day is invalid"
+                    }
+                } else { // LEAP YEAR
+                   if (day !in 1 .. 29) {
+                       message = "$position day is invalid"
+                   }
+                }
+            }
+            return message
+        }
+        fun beforeCheck(dateBefore: Date, date: Date, output: String) : String {
+            var message = ""
+            if (dateBefore.before(date)) {
+                message += output
+            }
+            return message
+        }
+
+        // Collecting data from activity
+        val title = findViewById<EditText>(R.id.editTextTitle).text.toString()
+        val startDay = findViewById<EditText>(R.id.editStartDay).text.toString()
+        val startMonth = findViewById<EditText>(R.id.editStartMonth).text.toString()
+        val startYear = findViewById<EditText>(R.id.editStartYear).text.toString()
+        val endDay = findViewById<EditText>(R.id.editEndDay).text.toString()
+        val endMonth = findViewById<EditText>(R.id.editEndMonth).text.toString()
+        val endYear = findViewById<EditText>(R.id.editEndYear).text.toString()
+
+        // Check for anything empty
+        if (title.isEmpty()) {
+            Toast.makeText(applicationContext, "The module cannot be empty", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (startDay.isEmpty() || startMonth.isEmpty() || startYear.isEmpty()) {
+            Toast.makeText(applicationContext, "The start date cannot be empty", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (endDay.isEmpty() || endMonth.isEmpty() || endYear.isEmpty()) {
+            Toast.makeText(applicationContext, "The end date cannot be empty", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        // Validation on each individual number
+        var currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        var message = validation(parseInt(startDay), parseInt(startMonth), parseInt(startYear), "Start", currentYear)
+        if (message.isNotEmpty()) {
+            Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        message = validation(parseInt(endDay), parseInt(endMonth), parseInt(endYear), "End", currentYear)
+        if (message.isNotEmpty()) {
+            Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        // Getting data to Date, to compare if before dates
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.UK)
+
+        val sd = sdf.parse("$startDay/$startMonth/$startYear")
+        val ed = sdf.parse("$endDay/$endMonth/$endYear")
+        val current = sdf.parse(sdf.format(Date()))
+
+        // validation on before dates
+        message = beforeCheck(sd, current, "The start date cannot be in past")
+        if (message.isNotEmpty()) {
+            Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+            return
+        }
+        message = beforeCheck(sd, current, "The end date cannot be in past")
+        if (message.isNotEmpty()) {
+            Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+            return
+        }
+        message = beforeCheck(ed, sd, "The start date cannot be after date")
+        if (message.isNotEmpty()) {
+            Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        // Output to Survey, start CreateQuestionActivity
+        val startDate = "$startDay/$startMonth/$startYear"
+        val endDate = "$endDay/$endMonth/$endYear"
 
         val newSurvey = Survey(-1, globalId, title, startDate, endDate)
 
