f285898f165db717a59d86ad8999295425fd9996
Oscar Soanes <oscarsoanes@gmail.com>
2022-12-02 21:11:29 +0000

    Question visual, no functionality, cannot access

---
+++ app/src/main/java/com/example/surveyapp/studentController/StudentAnswerQuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.surveyapp.studentController
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.surveyapp.R
+
+class StudentAnswerQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_answer_question)
+    }
+}
